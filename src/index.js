8f28f5f21e8aa74bbd52d6522b9b77d0a40dfc33
fazt <fazt@faztweb.com>
2020-12-06 16:59:21 -0500

    json response updated

--- src/index.js
+++ src/index.js
@@ -12,9 +12,10 @@ app.set("port", process.env.PORT || 3000);
 app.get("/weather/:city", async (req, res) => {
   const URL = `https://api.openweathermap.org/data/2.5/weather?q=${req.params.city}&appid=${config.secret}`;
   const response = await axios.get(URL);
-  console.log(response.data);
 
-  res.json(response.data);
+	const {weather, main} = response.data;
+
+  res.json({weather, main});
 });
 
 // static files
